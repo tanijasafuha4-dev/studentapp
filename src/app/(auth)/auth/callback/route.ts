b823df25efafda1a7513265a9cbfece19e3187b0
tanijasafuha4-dev <tanijasafuha4@gmail.com>
2026-09-15 19:02:01 +0800

    Update route.ts

--- src/app/(auth)/auth/callback/route.ts
+++ src/app/(auth)/auth/callback/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 // The client you created from the Server-Side Auth instructions
 import { createClient } from "@/lib/supabase/server";
+export const runtime = 'edge';
 
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
