7ba0410d57db9d88f3b144995e6777bf1928314b
Kevin Shek <tanijasafuha4@gmail.com>
2026-09-16 18:28:29 +0800

    Update page.tsx

--- src/app/(years)/years/page.tsx
+++ src/app/(years)/years/page.tsx
@@ -1,4 +1,5 @@
 import React, { Suspense } from "react";
+import DeleteYearButton from "@/components/years/delete-year-button";
 import PageLoader from "@/components/shared/page-loader";
 import { redirect } from "next/navigation";
 import {
