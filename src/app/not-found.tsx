3936a9ff590ac5d29b47d01cff7c9a2023c26a72
tanijasafuha4-dev <tanijasafuha4@gmail.com>
2026-09-16 10:52:53 +0800

    Update not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { LanguageProvider } from "@/contexts/language-context";
 import { useTranslation } from "@/hooks/use-translation";
 import { Button } from "@/components/ui/button";
 import { Info } from "lucide-react";
